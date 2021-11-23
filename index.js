8347fed24c2a57af71f30d40feb8afd455bbad45
Dakota Castle <barrettcastle@gmail.com>
2021-11-22 22:52:14 -0500

    commit tree 90638936d9d4560de1465414773e8e93e07e7e3c

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someVaule){
+  if(someVaule <= 400) {
+    return 'This one is on me!'
+  }
+  else if(someVaule > 2500) {
+    return 'No can do.'
+  }
+  else if(someVaule > 2000) {
+    return 'I will gladly take your thirty bucks.'
+   
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+  switch(charm) {
+      case "generous" :
+        return "Thank you so much."
+      case "not as generous" :
+        return "Thank you."
+      case "thanks for everything" :
+        return "Bye."
+  }
+  return charm
 }
